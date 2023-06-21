82d67a8b17c01157ccd63d14118be658f4f33e00
wangfc <wangfc@hzwesoft.com>
2023-06-21 18:57:14 +0800

    Initial commit

---
+++ spring-cloud-web/src/main/java/com/summer/srpingcloudweb/SrpingCloudWebApplication.java
@@ -0,0 +1,13 @@
+package com.summer.srpingcloudweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingCloudWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingCloudWebApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-web/src/test/java/com/summer/srpingcloudweb/SrpingCloudWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.srpingcloudweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingCloudWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
